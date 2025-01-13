cf177b01e2d3731589373bfee66a2555cfd8c727
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-01-13 14:16:34 +0100

    app.js basics

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({message: "Ok, working..."});
+})
+
+app.use((err, req, res, next) => {
+    console.log("Something wrong...",err.message, err.stack);
+    return res.status(500).json({message: err.message});
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"));
